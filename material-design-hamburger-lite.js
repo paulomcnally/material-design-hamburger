3421a3110d04cde42811057d62468351643f53cc
Paulo McNally <paulomcnally@gmail.com>
2014-12-12 01:10:35 -0600

    fix merge

--- material-design-hamburger-lite.js
+++ material-design-hamburger-lite.js
@@ -1,23 +1,8 @@
-<<<<<<< HEAD:src/material-design-hamburger.js
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.materialDesignHamburger=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-
-  module.exports = materialDesignHamburger;
-
-  function materialDesignHamburger() {
-    'use strict';
-
-    document.querySelector('.material-design-hamburger__icon').addEventListener(
-        'click',
-        function() {
-      var child = this.childNodes[1].classList;
-
-=======
 (function() {
   'use strict';
   document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.material-design-hamburger__icon').addEventListener('click', function() {
       var child = this.childNodes[0].classList;
->>>>>>> develop:material-design-hamburger-lite.js
       if (child.contains('material-design-hamburger__icon--to-arrow')) {
         child.remove('material-design-hamburger__icon--to-arrow');
         child.add('material-design-hamburger__icon--from-arrow');
@@ -27,12 +12,5 @@
         child.add('material-design-hamburger__icon--to-arrow');
       }
     });
-<<<<<<< HEAD:src/material-design-hamburger.js
-  }
-
-},{}]},{},[1])(1)
-});
-=======
   });
 })();
->>>>>>> develop:material-design-hamburger-lite.js
